294b869f339e033dde556bf2dbc613d4083a4903
hanxiao <hanxiao84322@163.com>
2023-12-22 14:09:46 +0800

    no message

--- console/controllers/DeliveryOrderTaskController.php
+++ console/controllers/DeliveryOrderTaskController.php
@@ -225,7 +225,7 @@ class DeliveryOrderTaskController extends Controller
                         if (!empty($results)) {
                             foreach ($results as $re) {
                                 $reArr = json_decode($re, true);
-                                if (!is_array($reArr)) {
+                                if (is_array($reArr)) {
                                     $resList = array_merge($resList, $reArr);
                                 } else {
                                     var_dump($reArr);
