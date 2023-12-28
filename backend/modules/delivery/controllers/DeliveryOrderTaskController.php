d44c705f137afac376dd67ab151676395b6935d7
hanxiao <hanxiao84322@163.com>
2023-12-28 13:52:39 +0800

    no message

--- backend/modules/delivery/controllers/DeliveryOrderTaskController.php
+++ backend/modules/delivery/controllers/DeliveryOrderTaskController.php
@@ -113,7 +113,7 @@ class DeliveryOrderTaskController extends Controller
             $post = $this->request->post();
             $post['DeliveryOrderTask']['file_path'] = $filePath;
             $post['DeliveryOrderTask']['type'] = $post['type'];
-            $post['DeliveryOrderTask']['settlement_dimension'] = $post['settlement_dimension'];
+            $post['DeliveryOrderTask']['settlement_dimension'] = !empty($post['settlement_dimension']) ? $post['settlement_dimension'] : '';
             $post['DeliveryOrderTask']['order_type'] = !empty($post['order_type']) ? $post['order_type'] : '';
             $post['DeliveryOrderTask']['apply_time'] = date('Y-m-d H:i:s', time());
             $post['DeliveryOrderTask']['apply_username'] = \Yii::$app->user->getIdentity()['username'];
